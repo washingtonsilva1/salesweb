44c4434120f389609100e4544f19856c0ddc7825
Deivison Washington <deivisonwashington@hotmail.com>
2022-07-13 23:24:09 -0300

    Foreign key not null (referential integrity)

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SalesWeb.Services
 
         public void Insert(Seller seller)
         {
+            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
