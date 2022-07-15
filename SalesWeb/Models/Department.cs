92f109f9f5aefae66be864fbe9ce8d8ef8fd47d7
Deivison Washington <deivisonwashington@hotmail.com>
2022-07-15 12:17:47 -0300

    Pequenos ajustes

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWeb.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter a valid {0}")]
+        [Display(Name = "Department Name")]
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
